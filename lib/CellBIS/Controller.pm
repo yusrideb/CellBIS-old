4f3ccde5eff40a7385ff91388f72ff7a5d5f7084
Achmad Yusri Afandi <linuxer08@gmail.com>
2017-07-31 01:14:44 +0800

    Initial Release CellBIS::Controller Module

---
+++ lib/CellBIS/Controller.pm
@@ -0,0 +1,19 @@
+package CellBIS::Controller;
+use strict;
+use warnings;
+
+# Use Module :
+use Module::Load;
+use CellBIS::Routing;
+use Hash::Merge qw( merge );
+use CellBIS::Utils::Char;
+use CellBIS::Devel::QC;
+
+# Version :
+require CellBIS::Version;
+our $VERSION = $CellBIS::Version::VERSION;
+
+# Variable will be use :
+
+
+1;
