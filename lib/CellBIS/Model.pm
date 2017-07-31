5b14837131ab0b87838f1b5e539bdfd9ae1fdce4
Achmad Yusri Afandi <linuxer08@gmail.com>
2017-07-31 15:53:36 +0800

    Initial Release CellBIS::Model

---
+++ lib/CellBIS/Model.pm
@@ -0,0 +1,46 @@
+package CellBIS::Model;
+use strict;
+use warnings FATAL => 'all';
+
+# Import Module :
+use Hash::Merge qw( merge );
+use CellBIS::Utils::Char;
+use CellBIS::Devel::QC;
+
+# Version :
+our $VERSION = '0.1000';
+
+# Declare :
+our $_config = {};
+our $_models = {};
+sub _add;
+
+# Subroutine for config controller :
+# ------------------------------------------------------------------------
+sub _config {
+    # Define parameter subroutine :
+    my ($self, $config) = @_;
+    Carp::croak(q{$config is required})
+        unless defined $config && ref($config) eq 'HASH';
+    my %set_config = %{ merge($_config, $config) };
+    $_config = \%set_config;
+}
+# End of Subroutine for config controller.
+# ===========================================================================================================
+
+# Subroutine for add model :
+# ------------------------------------------------------------------------
+sub _add {
+    my $self = shift;
+    my %data = ();
+    $data{$_[0]} = $_[1];
+    my %add_models = %{ merge($_models, \%data) };
+    $_models = \%add_models;
+}
+# End of Subroutine for add model.
+# ===========================================================================================================
+
+
+1;
+__END__
+#
